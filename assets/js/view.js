6b7f6e1aede77b8a35345b99e8e0b5dda8941368
Dylan Shine <dylanshinenyc@gmail.com>
2015-03-12 20:50:10 -0400

    Add updateGif function in view

--- assets/js/view.js
+++ assets/js/view.js
@@ -6,3 +6,16 @@ View.prototype.updateDisplay = function(player) {
   player.$sprite.css('top', player.y);
   player.$sprite.css('left', player.x);
 }
+
+View.prototype.updateGif = function(player, action) {
+  if (player[action + "ing"]) {
+    switch (player.facing) {
+      case "right":
+        player.$sprite.css('background-image', player.sprite[action + "Right"]);
+        break;
+      case "left":
+        player.$sprite.css('background-image', player.sprite[action + "Left"]);
+        break;
+    }
+  }
+}
