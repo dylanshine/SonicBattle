714d772def1f44a86bdd196f4c54e81636d88e6c
Dylan Shine <dylanshinenyc@gmail.com>
2015-03-12 20:53:36 -0400

    Add loop to game controller

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,3 +5,19 @@ function Game(players) {
   this.players = players;
   this.platforms = [];
 }
+
+Game.prototype.loop = function() {
+  var that = this;
+  this.players.forEach(function(player) {
+    player.move();
+    player.dead();
+    that.scoreUpdate(player);
+    that.winner(player);
+    that.roundMap(player);
+    that.jumping(player);
+    view.updateDisplay(player);
+    if (player.jumping === false) {
+      that.findGroundUnderPlayer(player);
+    }
+  });
+}
